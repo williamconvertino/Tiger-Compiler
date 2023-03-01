2b38ea8991de797fe418c7df8c2e74a63ba09ddc
SaraBeatriz <saraschwarz8@gmail.com>
2023-03-01 16:36:05 -0500

    adding automated testing

---
+++ automate.py
@@ -0,0 +1,35 @@
+import os
+# assign directory
+directory = 'testcases'
+ 
+# iterate over files in
+# that directory
+
+printMain = '\\n";\n'
+print1 = 'print "AST===========>\\n";\n'
+text1 = '(PrintAbsyn.print(TextIO.stdOut, Parse.parse "'
+text1_2 = '"));\n'
+print2 = 'print "TYPECHECKING=============>\\n";\n'
+text2 = 'Main.compile "'
+text2_2 = '";\n'
+
+
+# Append-adds at last
+file1 = open("printTests.txt", "w") # append mode
+file1.write('CM.make "typechecking/sources.cm";\n')
+for filename in os.listdir(directory):
+    if ("test" in filename and "sara" not in filename):
+        f = os.path.join(directory, filename)
+        # checking if it is a file
+        if os.path.isfile(f):
+            file1.write('print "'+ str(f) + printMain)
+            file1.write(print1)
+            file1.write(text1 + str(f) + text1_2)
+            file1.write(print2)
+            file1.write(text2 + str(f) + text2_2)
+
+
+
+
+file1.close()
+
